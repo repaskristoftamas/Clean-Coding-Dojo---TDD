1f4f7f171c632118f34e02efd93cc705aee578cc
Kristóf Répás <repas.kristof.tamas@protonmail.com>
2025-02-25 17:29:55 +0100

    Added custom exceptions for minimum and maximum purchases

---
+++ TDD_Coding_Dojo/TDD_Coding_Dojo/Exceptions/MinimumPurchaseException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TDD_Coding_Dojo.Exceptions
+{
+    /// <summary>
+    /// Thrown when any item is below the minimum required quantity.
+    /// </summary>
+    public class MinimumPurchaseException : Exception
+    {
+        public MinimumPurchaseException()
+        {
+        }
+
+        public MinimumPurchaseException(string message)
+            : base(message)
+        {
+        }
+
+        public MinimumPurchaseException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
